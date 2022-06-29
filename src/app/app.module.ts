4e71ec1f751d5bd743122de3d0d9d8b9c8750f42
marcoschultznunes <marco_sn@hotmail.com>
2022-06-29 18:12:48 -0300

    Project initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoInputComponent } from './todo-input/todo-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoInputComponent
   ],
   imports: [
     BrowserModule
